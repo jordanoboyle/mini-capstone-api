40132452be4c14f80c2ad389c444db65672bf63f
jordanoboyle <jmcmcode@gmail.com>
2024-05-14 12:57:34 -0600

    test render worked

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,16 @@
 class ProductsController < ApplicationController
 
   def all
+    # test render
+    product = Product.first
     render json: {
-      message: "hello there"
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      image_url: product.image_url,
+      description: product.description,
+      created_at: product.created_at,
+      updated_at: product.updated_at
     }
   end
 
