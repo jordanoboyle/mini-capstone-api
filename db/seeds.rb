204e465e7f884b65d289a707ef8b49e33a5f3e85
jordanoboyle <jmcmcode@gmail.com>
2024-05-14 12:53:34 -0600

    added a seed data creation spread just to get the database populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# 2.times do
+#   product = Product.new(name: "Goku Mug", price: 10, image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXaR8B3xDXlLqOo5Y1q8c2T1lV6K-p31G3wYRw8uEQ4g&s", description: "Ultra Instinct mug")
+#   product.save
+
+#   product = Product.new(name: "Totoro Hoody", price: 68, image_url: "https://enez76gwp29.exactdn.com/wp-content/uploads/2020/12/redirect12092020091218-1.jpg", description: "Totoro sleeping with his little friend. Cream white hoodie.")
+#   product.save
+  
+#   product = Product.new(name: "Luffy hoodie", price: 55, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiZJezsIzQgQ7Lgm0dBmw3-Yng7reeleLI9PkzjVJnQw&s", description: "Monkey D Luffy Hoodie")
+#   product.save
+# end
