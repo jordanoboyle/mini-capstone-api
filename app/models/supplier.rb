79d4b843f0ad9a06160f07dc20ad66f48c47ab5b
jordanoboyle <jmcmcode@gmail.com>
2024-05-22 10:54:40 -0600

    belongs_to and has_many, generated Suppliers Controller and jbuilder

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,11 @@
 class Supplier < ApplicationRecord
+
+  #has_many :products
+  # ABOVE IS THE SIMPLE SHORTCUT AND SYNTAX OF BELOW
+  def products
+    Product.where(supplier_id: id)
+  end
+
+
+
 end
