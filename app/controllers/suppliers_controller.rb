d04bc9a8e6a694238eeff32160968d92257ec65c
jordanoboyle <jmcmcode@gmail.com>
2024-05-22 21:54:08 -0600

    happy sad create, create action dynamic, validations, country_code_number

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,35 @@
 class SuppliersController < ApplicationController
+
+  def create
+    @supplier = Supplier.new(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+    )
+    if @supplier.save
+      render template: "suppliers/show"
+    else
+      render json: {ERRORS: @supplier.errors.full_messages}
+    end
+  end
+
+  def index
+    render json: {message: "jim is dead"}
+  end
+
+  def show
+    render json: {message: "jim is dead"}
+  end
+
+  def update
+    render json: {message: "jim is dead"}
+  end
+  
+  def destroy
+    render json: {message: "jim is dead"}
+  end
 end
+
+
+
+
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,10 +1,19 @@
 class Supplier < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, length: { minimum: 7 }
+  validates :phone, presence: true
+
+  
 
   has_many :products
   # ABOVE IS THE SIMPLE SHORTCUT AND SYNTAX OF BELOW
   # def products
   #   Product.where(supplier_id: id)
   # end
+  def country_code_number
+    pretty = "(+1) #{phone_number}"
+    return pretty
+  end
 
 
 
