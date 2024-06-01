68c9606ef39e3fa02dc7e6e9caca3399c390c38c
jordanoboyle <jmcmcode@gmail.com>
2024-06-01 14:39:08 -0600

    validates supplier_id presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0}
   validates :description, length: { in: 20...501}
+  validates :supplier_id, presence: true
 
   has_many :images
   belongs_to :supplier
