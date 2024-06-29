1e36532c0af4eee29816e5aa3dd0c11b460c4df8
jordanoboyle <jmcmcode@gmail.com>
2024-06-29 13:47:07 -0600

    seeds rearrange 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,34 @@
 #   end
 
 #This is meant to follow the instruction above. rails db:reset rails db:seed will clean local data base. 
+# Users seed:
+1.times do
+  User.create(name: "Luke Skywalker", email: "luke@email.com", password: "password", password_confirmation: "password", admin: true)
+  User.create(name: "John Wick", email: "john@email.com", password: "password", password_confirmation: "password", admin: false)
+  User.create(name: "Jordan O'Boyle", email: "jordan@email.com", password: "password", password_confirmation: "password", admin: false)
+  User.create(name: "Harley Quin", email: "harley@email.com", password: "password", password_confirmation: "password", admin: false)
+  User.create(name: "James Kirk", email: "james@email.com", password: "password", password_confirmation: "password", admin: false)
+end
+
+# CATEGORY SEED
+1.times do
+  Category.create(name: "clothing") #1
+  Category.create(name: "digital media") #2
+  Category.create(name: "video game") #3
+  Category.create(name: "anime") #4
+  Category.create(name: "cosplay") #5
+  Category.create(name: "plateware") #6
+  Category.create(name: "sci-fi") #7
+end 
+
+# SUPPLIER SEED
+supplier = Supplier.new(name: "Box Lunch", email: "person@boxlunch.com", phone_number: "4445556666")
+supplier.save
+supplier = Supplier.new(name: "GameStop", email: "person@gamestop.com", phone_number: "3334445555")
+supplier.save
+supplier = Supplier.new(name: "Etsy", email: "person@etsy.com", phone_number: "5556667777")
+supplier.save
+
 #Products Seed:
 1.times do
   Product.create(name: "Goku Mug", price: 10, description: "Goku in ultra instinct for the morning.", inventory: 50, supplier_id: 1) #1
@@ -24,7 +52,8 @@
   Product.create(name: "Rogue Fable III", price: 15, description: "Like Rogues Likes? Go to town!", inventory: 50, supplier_id: 3) #11
   Product.create(name: "Calcifer Hoody", price: 40, description: "Everyone's favorite fire demon", inventory: 50, supplier_id: 3) #12
 end
-IMAGES TABLE SEED
+
+# IMAGES TABLE SEED
 1.times do
   Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXaR8B3xDXlLqOo5Y1q8c2T1lV6K-p31G3wYRw8uEQ4g&s", product_id: 1)
   Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf3cLQ0YKtXaXWoR8S_QQc_FlbFuA9GaBqxDBsDgJLMA&s", product_id: 1)
@@ -51,27 +80,6 @@ IMAGES TABLE SEED
   Image.create(url: "https://i.etsystatic.com/37825678/r/il/741554/4327915673/il_570xN.4327915673_2lbe.jpg", product_id: 12)
   Image.create(url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJaa6U0uYAd-IJcNIzTPPwixgfYmecYLGDA0Moe1UlXQ&s", product_id: 12)
 end
-
-Users seed:
-1.times do
-  User.create(name: "Luke Skywalker", email: "luke@email.com", password: "password", password_confirmation: "password", admin: true)
-  User.create(name: "John Wick", email: "john@email.com", password: "password", password_confirmation: "password", admin: false)
-  User.create(name: "Jordan O'Boyle", email: "jordan@email.com", password: "password", password_confirmation: "password", admin: false)
-  User.create(name: "Harley Quin", email: "harley@email.com", password: "password", password_confirmation: "password", admin: false)
-  User.create(name: "James Kirk", email: "james@email.com", password: "password", password_confirmation: "password", admin: false)
-end
-
-
-CATEGORY SEED
-1.times do
-  Category.create(name: "clothing") #1
-  Category.create(name: "digital media") #2
-  Category.create(name: "video game") #3
-  Category.create(name: "anime") #4
-  Category.create(name: "cosplay") #5
-  Category.create(name: "plateware") #6
-  Category.create(name: "sci-fi") #7
-end 
 
 #CATEGORY_PRODUCT SEED
 1.times do
@@ -100,7 +108,6 @@ end
   CategoryProduct.create(product_id: 12, category_id: 1)
   CategoryProduct.create(product_id: 12, category_id: 4)
 end
-
 
 
 
